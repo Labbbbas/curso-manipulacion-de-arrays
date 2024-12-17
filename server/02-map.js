05848d51b7c5b898ea15a27cb7b1dee1a6901694
FernandoLabastida <labastidafernando@comunidad.unam.mx>
2024-12-17 13:29:34 -0600

    Segunda clase: ejercicio con map

--- server/02-map.js
+++ server/02-map.js
@@ -7,7 +7,16 @@ for (let index = 0; index < letters.length; index++) {
     newArray.push(element + '++');
 }
 console.log('original', letters);
-console.log('new', newArray);
+console.log('for ', newArray);
 
 // Método con map
-console.log(letters.map(item => item + '++'));
+console.log('map', letters.map(item => item + '++'));
+
+// Ejercicio:
+function multiplyElements(array) {
+    return array.map(item => item * 2);
+}
+
+const numbers = [2, 4, 6, -2, 10];
+console.log('original numbers', numbers);
+console.log('multiplied numbers', multiplyElements(numbers));
